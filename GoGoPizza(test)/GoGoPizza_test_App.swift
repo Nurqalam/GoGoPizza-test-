b7a77f6d002725995329d0be8248cdbb5a96da26
Nurqalam <tnurkalam@gmail.com>
2022-05-20 14:39:32 +0500

    Initial Commit

---
+++ GoGoPizza(test)/GoGoPizza_test_App.swift
@@ -0,0 +1,17 @@
+//
+//  GoGoPizza_test_App.swift
+//  GoGoPizza(test)
+//
+//  Created by Nurqalam on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GoGoPizza_test_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
